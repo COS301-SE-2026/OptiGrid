531631c737fe1afacf829d8643850600601f8cb0
Abhay Rooplall <u24568792@tuks.co.za>
2026-04-29 20:08:12 +0200

    Added login controller

--- backend/core/src/controllers/user_auth.controller.ts
+++ backend/core/src/controllers/user_auth.controller.ts
@@ -26,3 +26,24 @@ export const signup = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const login = async (req: Request, res: Response) => {
+    try{
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "Email and password are required fields."});
+        }
+        const user = await authService.login(email, password);
+        return res.status(200).json({
+            message: 'Login successful',
+            user,
+        });
+    }
+    catch(error: any){
+        if(error.message === "Invalid email or password"){
+            return res.status(400).json({message: error.message});
+        }
+        console.error("Login error:", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+}
