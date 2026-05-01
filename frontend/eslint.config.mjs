0bca73781b429977fc5b9d7008b2716906c47065
Hamdaan-Mirza <hamdaanmirza1@gmail.com>
2026-05-01 21:32:29 +0200

    fix: made changes to make lint n build work

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,26 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**', '.next/**', '.storybook/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
