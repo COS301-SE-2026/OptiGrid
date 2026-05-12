e214e35d67ae627176414caf2abc29b10437ee1e
Hamdaan-Mirza <hamdaanmirza1@gmail.com>
2026-05-12 11:40:13 +0200

    feat: create building api and validate fields

--- backend/core/src/services/building.services.ts
+++ backend/core/src/services/building.services.ts
@@ -4,7 +4,7 @@ import { BuildingType } from '@prisma/client';
 // handle building creation, updating, deletion, and others here
 export interface buildingPayload {
   tenant_id: string; 
-  building_name: string; 
+  building_name: string;
   building_type?: BuildingType; 
   square_footage?: number;
   physical_address?: string;
@@ -12,6 +12,34 @@ export interface buildingPayload {
   max_occupancy?: number;
 }
 
-export const createBuilding = async (payload: buildingPayload) => {
-  //Need to complete
+export const createBuilding = async (
+  userId: string, 
+  payload: buildingPayload
+) => {
+  // we ensure we have a prisma transaction for data integrity
+  return await prisma.$transaction(async (tx) => {
+    
+    //create the building row 
+    const newBuilding = await tx.building.create({
+      data: {
+        tenant_id: payload.tenant_id,
+        building_name: payload.building_name,
+        building_type: payload.building_type || 'Residential', 
+        square_footage: payload.square_footage,
+        physical_address: payload.physical_address,
+        timezone: payload.timezone || 'UTC',
+        max_occupancy: payload.max_occupancy
+      },
+    });
+
+    // grant access to the user who created the building
+    await tx.userBuildingAccess.create({
+      data: {
+        user_id: userId,
+        building_id: newBuilding.building_id,
+      },
+    });
+
+    return newBuilding;
+  });
 };
