8def89d8ae45f98f5c666371c162e01ba637b173
Hamdaan-Mirza <hamdaanmirza1@gmail.com>
2026-06-18 11:30:49 +0200

    feat:unit tests for controller file

---
+++ tests/unit/backend/contact.controller.test.ts
@@ -0,0 +1,60 @@
+import { mock } from "node:test";
+import { handleSubmit } from "../../../backend/core/src/controllers/contact.controller.ts"
+import { contactService } from "../../../backend/core/src/services/contact.services.ts"
+import { Request, Response } from "express"
+
+jest.mock("../../../backend/core/src/services/contact.services.ts");
+
+describe("Contact-Us page Controller", () => {
+    let req: Partial<Request>;
+    let resp: Partial<Response>;
+    let mockstatus = jest.mock;
+    let json = jest.mock;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        json = jest.fn();
+        mockstatus = jest.fn().mockReturnValue({json: json});
+
+        resp = {
+            status: mockstatus,
+        };
+    });
+
+    it("should_return_an_200_when_everything_is_valid", () => {
+        //arrange
+        req = {
+            body: {
+                inquiryType: "Building",
+                subject: "Testing",
+                message: "we need this message to be more than 10 characters long",
+            },
+        };
+        //act
+        (contactService.sendMail as jest.Mock).mockResolvedValue({id: "email-111"});
+        //assert
+        expect(contactService.sendMail).toHaveBeenCalledWith(req.body, undefined);
+        expect(mockstatus).toHaveBeenCalledWith(200);
+        expect(json).toHaveBeenCalledWith({
+            success: true,
+            message: "Recieved the the ticket",
+            id: "email-111",
+        });
+        
+    });
+
+    it("should_return_400_if_field_missing", async () => {
+        //arrange
+        req = {
+            body: {
+                inquiryType: "Building",
+                subject: "Testing",
+            },
+        };
+        //acr and assert
+        await handleSubmit(req as Request, resp as Response);
+        expect(contactService.sendMail).not.toHaveBeenCalled();
+        expect(mockstatus).toHaveBeenLastCalledWith(400);
+        expect(json).toHaveBeenCalledWith(expect.objectContaining({success: false}));
+    });
+})
