a4dc14d207a485f5c00aa7f45a241c8e739def1b
TalifhaniSeaba <u23657350@tuks.co.za>
2026-06-19 13:54:55 +0200

    manual intro

---
+++ frontend/app/user_manual/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function UserManualPage(){
+
+    return (
+   
+    <main>
+
+        <h1>Optigrid - User Manual</h1>
+
+        <section>
+            <h2>1. Introduction</h2>
+            <p>Optigrid is a centralised, intelligent energy management system that integrates data ingestion, predictive analytics, and optimisation insights to improve operational efficiency and sustainability. </p>
+
+<ul>
+    <li>Monitors real-time and historical energy consumption across multiple buildings.</li>
+    <li>Predicts future energy demand.</li>
+    <li>Detects inefficiencies and abnormal usage patterns.</li>
+    <li>Recommends optimisation strategies. </li>
+</ul>
+        </section>
+    </main>
+
+
+
+
+    )
+
+
+
+
+
+
+
+
+}
