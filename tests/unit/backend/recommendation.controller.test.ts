d8ef31562ccb83ec7b7874403c24d0feb599ff6c
Hamdaan-Mirza <hamdaanmirza1@gmail.com>
2026-08-17 15:14:50 +0200

    added tests for view controller

--- tests/unit/backend/recommendation.controller.test.ts
+++ tests/unit/backend/recommendation.controller.test.ts
@@ -1,5 +1,5 @@
-import { applyRecommendationController } from "../../../backend/core/src/controllers/recommendation.controller";
-import { applyRecommendation } from "../../../backend/core/src/services/recommendation.service";
+import { applyRecommendationController, viewRecommendationController } from "../../../backend/core/src/controllers/recommendation.controller";
+import { applyRecommendation, viewRecommendationService } from "../../../backend/core/src/services/recommendation.service";
 import { Request, Response } from "express";
 
 jest.mock("../../../backend/core/src/services/recommendation.service");
@@ -141,5 +141,104 @@ describe("Recommendation Controller Unit Tests", () => {
             status: "error", 
             message: "Internal server error" 
         }));
+    });
+
+    describe("View Recommendation Controller Unit Tests", () => {
+        it("should_return_200", async () => {
+            req = {
+                user: {
+                    userId: "user123"
+                } as any,
+                params: {
+                    building_id: "550e8400-e29b-41d4-a716-446655440000"
+                },
+                query: {
+                    status: "Pending",
+                    limit: "10"
+                }
+            };
+            const data = [{
+                recommendation_id: "rec-123"
+            }];
+            (viewRecommendationService as jest.Mock).mockResolvedValue(data);
+            //act
+            await viewRecommendationController(req as Request, resp as Response);
+            //assert
+            expect(viewRecommendationService).toHaveBeenCalledWith("user123", "550e8400-e29b-41d4-a716-446655440000", "Pending", 10);
+            expect(mockstatus).toHaveBeenCalledWith(200);
+            expect(json).toHaveBeenCalledWith({
+                status:"success",
+                data: data
+            });
+        });
+
+        it("should_return_401", async () => {
+            req = {
+                user: undefined,
+                params: {
+                    building_id: "550e8400-e29b-41d4-a716-446655440000"
+                },
+                query: {
+                    status: "Pending",
+                    limit: "10"
+                }
+            };
+            //act
+            await viewRecommendationController(req as Request, resp as Response);
+            //assert
+            expect(mockstatus).toHaveBeenCalledWith(401);
+            expect(json).toHaveBeenCalledWith({
+                status:"error",
+                message: "Unauthorized"
+            });
+            expect(viewRecommendationService).not.toHaveBeenCalled();
+        });
+
+        it("should_return_400_ZOD_ERROR", async () => {
+            req = {
+                user: {
+                    userId: "user123"
+                } as any,
+                params: {
+                    building_id: "invalid"
+                },
+                query: {
+                    status: "Pending",
+                    limit: "10"
+                }
+            };
+            //act
+            await viewRecommendationController(req as Request, resp as Response);
+            //assert
+            expect(mockstatus).toHaveBeenCalledWith(400);
+            expect(json).toHaveBeenCalledWith({
+                status:"error",
+            });
+            expect(viewRecommendationService).not.toHaveBeenCalled();
+        });
+
+        it("should_return_500", async () => {
+            req = {
+                user: {
+                    userId: "user123"
+                } as any,
+                params: {
+                    building_id: "550e8400-e29b-41d4-a716-446655440000"
+                },
+                query: {
+                    status: "Pending",
+                    limit: "10"
+                }
+            };
+            (viewRecommendationService as jest.Mock).mockRejectedValue(new Error("Internal"));
+            //act
+            await viewRecommendationController(req as Request, resp as Response);
+            //assert
+            expect(mockstatus).toHaveBeenCalledWith(400);
+            expect(json).toHaveBeenCalledWith({
+                status:"error",
+                message: "Internal Server Error"
+            });
+        });
     });
 });
