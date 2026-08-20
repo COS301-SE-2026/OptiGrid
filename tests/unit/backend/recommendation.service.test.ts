b10af87dd0193b67a13001f05f4760be076e96a6
Hamdaan-Mirza <hamdaanmirza1@gmail.com>
2026-08-20 09:45:38 +0200

    feat: services unti tests for updateTariff enpoint

--- tests/unit/backend/recommendation.service.test.ts
+++ tests/unit/backend/recommendation.service.test.ts
@@ -1,4 +1,4 @@
-import { applyRecommendation, viewRecommendationService } from '../../../backend/core/src/services/recommendation.service';
+import { applyRecommendation, viewRecommendationService, updateTariffService } from '../../../backend/core/src/services/recommendation.service';
 import prisma from '../../../backend/core/src/lib/prisma';
 import { analyticsQueue } from '../../../backend/core/src/services/bullmq';
 
@@ -13,6 +13,14 @@ jest.mock('../../../backend/core/src/lib/prisma', () => ({
             update: jest.fn(),
             findMany: jest.fn(),
         },
+        building: {
+            findUnique: jest.fn(),
+        },
+        utilityTariff: {
+            findFirst: jest.fn(),
+            update: jest.fn(),
+            create: jest.fn(),
+        }
     },
 }));
 jest.mock('../../../backend/core/src/services/bullmq', () => ({
@@ -125,6 +133,63 @@ describe("Recommendation Services Unit Tests", () => {
             await expect(viewRecommendationService("user-123", "building-123"))
             .rejects.toThrow("Access Denied");
             expect(prisma.optimisationRecommendation.findMany).not.toHaveBeenCalled();
+        })
+    });
+
+    describe("Update Tariff Rates Unit Tests", () => {
+        it("shiuld_create_tariff_if_noy_exist", async () => {
+            (prisma.building.findUnique as jest.Mock).mockResolvedValue({ building_id: "building123" });
+            (prisma.userBuildingAccess.findFirst as jest.Mock).mockResolvedValue({ 
+                user_id: "user-123", 
+                building_id: "building123" 
+            });
+
+            (prisma.utilityTariff.findFirst as jest.Mock).mockResolvedValue(null);
+            (prisma.utilityTariff.create as jest.Mock).mockResolvedValue(true);
+
+            const payload = { 
+                peak_rate_zar: 0.15, 
+                off_peak_rate_zar: 0.08, 
+                season_name: "Summer" 
+            };
+            //act
+            const out = await updateTariffService("user-123", "building123", payload);
+            //asset
+            expect(out).toBe(true);
+            expect(prisma.utilityTariff.create).toHaveBeenCalledWith({
+                building_id: "building123",
+                peak_rate_zae: 0.15, 
+                off_peak_rate_zar: 0.08, 
+                season_name: "Summer"
+            });
+            expect(prisma.utilityTariff.update).not.toHaveBeenCalled();
+        });
+
+        it("should_throw_an_error_if_no_building_exists", async ()=>{
+            (prisma.building.findUnique as jest.Mock).mockResolvedValue(null);
+            const payload = { 
+                peak_rate_zar: 0.15, 
+                off_peak_rate_zar: 0.08, 
+                season_name: "Summer" 
+            };
+            //act n assert
+            await expect(updateTariffService("user-123", "building123", payload))
+            .rejects.toThrow("Building not found");
+            expect(prisma.userBuildingAccess.findFirst).not.toHaveBeenCalled();
+        });
+
+        it("should_throw_error_if_no_access", async ( ) => {
+            (prisma.building.findUnique as jest.Mock).mockResolvedValue({ building_id: "building123" });
+            (prisma.userBuildingAccess.findFirst as jest.Mock).mockResolvedValue(null);
+            const payload = { 
+                peak_rate_zar: 0.15, 
+                off_peak_rate_zar: 0.08, 
+                season_name: "Summer" 
+            };
+            //act n assert
+            await expect(updateTariffService("user-123", "build-123", payload))
+            .rejects.toThrow("Access Denied");
+            expect(prisma.utilityTariff.findFirst).not.toHaveBeenCalled();
         })
     });
 });
