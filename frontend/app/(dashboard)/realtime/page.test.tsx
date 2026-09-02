a0c0b86314ea2538228cf309c3f4fd338ab8fc1f
abdlrhmanhabish <abdlrhmanhabish03@gmail.com>
2026-09-03 00:57:19 +0200

    feat: update tutorials

--- frontend/app/(dashboard)/realtime/page.test.tsx
+++ frontend/app/(dashboard)/realtime/page.test.tsx
@@ -1,6 +1,7 @@
 import RealtimePage from "./page";
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@testing-library/jest-dom";
 
